23685d7f147c46cdf6a4293d5f4791806aaf86bc
gabriels3t <gabriels3t@hotmail.com>
2021-02-09 13:53:57 -0300

    Com o for

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+// Constante 
+#define NUMERO_DE_TENTATIVAS 5
 
 int main(){
     //  imprime o cabecalho do jogo
@@ -8,9 +10,41 @@ int main(){
     
     int numero_secreto = 42;
     int chute;
-    printf("Qual e o seu chute? ");
-    scanf("%d",&chute);
-    printf("Seu chute foi %d \n",chute);
+    int maior = chute > numero_secreto;
+    int menor = chute < numero_secreto;
+    int acertou = (chute == numero_secreto);
+
+    for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++)
+    { 
+        printf("Tentativa  %d de %d \n",i ,NUMERO_DE_TENTATIVAS);
+        printf("Qual e o seu chute? ");
+        scanf("%d",&chute);
+        printf("Seu chute foi %d \n",chute);
+        if (chute < 0 )
+        {
+            printf("Voce nao pode chutar numeros negativos \n"); 
+            i--;
+            continue;
+            // continue pula todo o bloco do codigo e volta para o inicio do for
+        }       
+        if(acertou){
+            printf("Voce acertou \n");
+            break;
+        }
+            
+        else if(maior)
+        {
+            printf("Seu Chute foi maior que o numero secreto\n");
+        }
+        else 
+        {
+            printf("Seu Chute foi menor que o numero secreto\n");
+        }
+        printf("Voce errou \n");
+         
+    }
+    
+    printf("Fim de jogo");
     system("pause");
 
 }
